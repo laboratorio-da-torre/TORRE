d4daa9ed954e9e070e5501985873e5c03382628a
machadomcatarina-dotcom <machado.mcatarina@gmail.com>
2026-09-16 19:33:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL =
-  "COLOCA_AQUI_O_TEU_PROJECT_URL";
+  "https://rvfdobjhfwjdvufwrirp.supabase.co/rest/v1/";
 
 const SUPABASE_KEY =
-  "COLOCA_AQUI_A_TUA_PUBLISHABLE_KEY";
+  "sb_publishable_aXh2UjT79AHJpjRXLVZeKA_UCRE96LL";
 
 const BUCKET = "images";
 
